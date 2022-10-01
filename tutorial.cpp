7121904fee236a1b1e4de3705e80d4bdf0d16d09
Danh Le <danhthanhle@usf.edu>
2022-09-30 23:38:04 -0400

    Polymorphism - virtual functions

--- tutorial.cpp
+++ tutorial.cpp
@@ -73,6 +73,11 @@ public:
         else
             std::cout << Name << " did not get promoted" << std::endl;
     }
+
+    virtual void Work()
+    {
+        std::cout << Name << " is checking email, task backlog, performing tasks..." << std::endl;
+    }
 };
 
 class Developer : public Employee
@@ -88,6 +93,10 @@ public:
     {
         std::cout << Name << " fixed bug using " << FavProgrammingLanguage << std::endl;
     }
+    void Work()
+    {
+        std::cout << Name << " is writing " << FavProgrammingLanguage << " code" << std::endl;
+    }
 };
 
 class Teacher : public Employee
@@ -102,19 +111,30 @@ public:
     {
         Subject = subject;
     }
+    void Work()
+    {
+        std::cout << Name << " is teaching " << Subject << std::endl;
+    }
 };
 
 // Our main function
 int main()
 {
     Developer dev1 = Developer("Danh Le", "Google", 31, "Python");
-
-    dev1.fixBug();
-    dev1.AskForPromotion();
+    // dev1.fixBug();
+    // dev1.AskForPromotion();
+    // dev1.Work();
 
     Teacher teach1 = Teacher("Mack", "coder school", 35, "math");
-    teach1.PrepareLesson();
-    teach1.AskForPromotion();
+    // teach1.PrepareLesson();
+    // teach1.AskForPromotion();
+    // teach1.Work();
+
+    Employee *e1 = &dev1;
+    Employee *e2 = &teach1;
+
+    e1->Work();
+    e2->Work();
 
     return 0;
 }
